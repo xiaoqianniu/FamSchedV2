2a1ab5560d41972432c121c6210f55fa7e6ca336
xiaoqianniu <xiaoqianniu@gmail.com>
2018-04-26 13:17:58 -0400

    create a view controller with a navibar and a segment and a tableview

--- FamSchedV2/ViewController.swift
+++ FamSchedV2/ViewController.swift
@@ -8,18 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //    MARK:- TableView DataSource Methods
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
     }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    
 
+    @IBAction func segmentSchedule(_ sender: UISegmentedControl) {
+    }
+    
 
+    @IBAction func addNewSchedule(_ sender: UIBarButtonItem) {
+    }
 }
 
